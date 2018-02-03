cf2dcb4417d79d1464246da359ed2c4fefa7737e
K2224 <k2224@student.jamk.fi>
2018-02-03 13:33:14 +0200

    finish state0

--- PhaserDemo/state0.js
+++ PhaserDemo/state0.js
@@ -1,37 +1,61 @@
-var demo = {}, centerX = 3500/2, centerY = 2000/2, adam, speed = 16;
-demo.state0 = function (){};
+var demo = {}, centerX = 1500 / 2, centerY = 1000 / 2, dragon, speed = 6;
+demo.state0 = function () {};
 demo.state0.prototype = {
-    preload: function(){
-        game.load.image('dragon', 'assets/sprites/frame-1.png');
+    preload: function () {
+        game.load.spritesheet('dragon', 'assets/spritesheets/dragonSheet.png', 939, 678);
+        game.load.image('background', 'assets/backgrounds/res2background.png');
     },
     
     
     create: function (){
+        game.physics.startSystem(Phaser.Physics.ARCADE);
         game.stage.backgroundColor = '#80ff80';
         console.log('state0');
         
         addChangeStateEventListeners();
+        game.world.setBounds(0, 0, 3556, 2000);
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        
+        var background = game.add.sprite(0, 0, 'background');
         
         dragon = game.add.sprite(centerX, centerY, 'dragon');
         dragon.anchor.setTo(0.5, 0.5);
+        dragon.scale.setTo(0.3, 0.3);
+        game.physics.enable(dragon);
+        dragon.body.collideWorldBounds = true;
+        dragon.animations.add('fly', [0, 1, 2, 3]);
+
+        
+        game.camera.follow(dragon);
+        game.camera.deadzone = new Phaser.Rectangle(centerX - 300, centerY - 200, 600, 400);
+        
     },
     
     
     update: function (){
         if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
             dragon.x += speed;
+            dragon.scale.setTo(0.3, 0.3);
+            dragon.animations.play('fly', 14, true);
         }
         else if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
             dragon.x -= speed;
+            dragon.scale.setTo(-0.3, 0.3);
+            dragon.animations.play('fly', 14, true);
         }
-        else if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
+        else{
+            dragon.animations.stop('fly');
+            dragon.frame = 0;
+        }
+        if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
             dragon.y -= speed;
+            /* if(dragon.y < 'y-koordinaatti'){
+                    dragon.y = 'sama y-koordinaatti' } */
         }
         else if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
             dragon.y += speed;
         }
-    },
+    }
 };
 
 
